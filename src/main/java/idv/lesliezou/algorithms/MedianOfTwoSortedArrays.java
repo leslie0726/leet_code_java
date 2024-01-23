524de53a082937671ced596f20b3c6f118ce26e0
Leslie Zou <event1511252@gmail.com>
2024-01-23 18:20:09 +0800

    [refactor] refactor MedianOfTwoSortedArrays

--- src/main/java/idv/lesliezou/algorithms/MedianOfTwoSortedArrays.java
+++ src/main/java/idv/lesliezou/algorithms/MedianOfTwoSortedArrays.java
@@ -1,23 +1,17 @@
 package idv.lesliezou.algorithms;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
 public class MedianOfTwoSortedArrays {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        List<Integer> result = new ArrayList<>();
-        for (int i : nums1) {
-            result.add(i);
-        }
-        for (int i : nums2) {
-            result.add(i);
-        }
-        Collections.sort(result);
-        if (result.size() % 2 == 0) {
-            return (result.get(result.size() / 2) + result.get(result.size() / 2 - 1)) / 2.0;
+        int[] result = new int[nums1.length + nums2.length];
+        System.arraycopy(nums1, 0, result, 0, nums1.length);
+        System.arraycopy(nums2, 0, result, nums1.length, nums2.length);
+        Arrays.sort(result);
+        if (result.length % 2 == 0) {
+            return (result[result.length / 2] + result[result.length / 2-1]) / 2.0;
         } else {
-            return result.get(result.size() / 2);
+            return result[result.length / 2];
         }
     }
 }
